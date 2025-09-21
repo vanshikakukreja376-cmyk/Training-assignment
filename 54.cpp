72c874ebcb3058165aa222529294d91653e87f41
vanshikakukreja376-cmyk <vanshikakukreja376@gmail.com>
2025-09-21 21:12:21 +0530

    54.cpp

---
+++ 54.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main() {
+    int rows = 5;
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            std::cout << j;
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
