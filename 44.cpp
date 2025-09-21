336fd5b38728bbe14130b137ddf0a4a1c2912335
vanshikakukreja376-cmyk <vanshikakukreja376@gmail.com>
2025-09-21 21:06:51 +0530

    44.cpp

---
+++ 44.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+using namespace std;
+ int main(){
+ 	// Ranges are taken by seeing question
+ 	for(int i=1;i<5;i++){
+ 		for(int j=1; j<7; j++){
+ 			cout<<i<<" ";
+		 }
+		 cout<<endl;
+	 }
+	 return 0;
+ }
