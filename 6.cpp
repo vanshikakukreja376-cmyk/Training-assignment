af25377acd3437a9497af939643405155d0770f6
vanshikakukreja376-cmyk <vanshikakukreja376@gmail.com>
2025-09-21 21:18:02 +0530

    6.cpp

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    // Input character
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    // Print ASCII code
+    cout << "The ASCII code of '" << ch << "' is: " << int(ch) << endl;
+
+    return 0;
+}
