7fd0fd5512bda7fdfbc3a3246b87022e6cf2b2d0
vanshikakukreja376-cmyk <vanshikakukreja376@gmail.com>
2025-09-21 20:41:02 +0530

    Create 18.cpp

---
+++ 18.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    int middle;
+
+    if ((a >= b && a <= c) || (a <= b && a >= c))
+        middle = a;
+    else if ((b >= a && b <= c) || (b <= a && b >= c))
+        middle = b;
+    else
+        middle = c;
+
+    cout << "The middle (second largest) number is: " << middle << endl;
+
+    return 0;
+}
