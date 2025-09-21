7c464d416e8872665965789e8088f26da7e69b11
vanshikakukreja376-cmyk <vanshikakukreja376@gmail.com>
2025-09-21 20:45:53 +0530

    Create 24.cpp

---
+++ 24.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, sum = 0;
+    cout << "Enter N: ";
+    cin >> N;
+
+    for(int i = 1; i <= N; i++) {
+    	if(i%2 != 0){
+    		sum += i;
+		}
+    }
+
+    cout << "Sum of first " << N << " odd natural numbers = " << sum << endl;
+    return 0;
+}
